36a93d9fa21a145dbd6e8a14fb7338b65b1d8a2e
Sergei <smyzin.work@gmail.com>
2021-09-11 13:30:40 +0300

    Remove all comments
     - remove all comments

--- Swift test/ViewController.swift
+++ Swift test/ViewController.swift
@@ -1,17 +1,9 @@
-//
-//  ViewController.swift
-//  Swift test
-//
-//  Created by Sergey Myzin on 11.09.2021.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
